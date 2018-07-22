d0e5440cf5697973afe92d73365b5b5d10c7a4e0
Julien LAVIE <jlavie@leadformance.com>
2018-07-22 15:45:41 +0200

    add(component): add material card for display some content

--- foodtruck/src/app/home/home.component.ts
+++ foodtruck/src/app/home/home.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-
+  img = 'assets/images/piza-3010062_1920.jpg';
   constructor() { }
 
   ngOnInit() {
