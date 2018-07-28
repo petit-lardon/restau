1501c840e9d172f2f5ba7469c4f68b9feb277c25
Julien LAVIE <jlavie@leadformance.com>
2018-07-28 23:45:31 +0200

    add model (content) + service (interface) + prepare component to display content

--- foodtruck/src/app/main-dashboard/main-dashboard.component.ts
+++ foodtruck/src/app/main-dashboard/main-dashboard.component.ts
@@ -1,39 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { map } from 'rxjs/operators';
-import { Breakpoints, BreakpointState, BreakpointObserver } from '@angular/cdk/layout';
+import { FirstService } from '../services/first.service'
+import {Content} from "../models/contentInterface";
 
 @Component({
   selector: 'app-main-dashboard',
   templateUrl: './main-dashboard.component.html',
   styleUrls: ['./main-dashboard.component.css']
 })
-export class MainDashboardComponent {
+export class MainDashboardComponent implements OnInit{
   /** Based on the screen size, switch from standard to one column per row */
-  cards = this.breakpointObserver.observe(Breakpoints.Handset).pipe(
-    map(({ matches }) => {
-      if (matches) {
-        return [
-          { title: 'Card 1', cols: 1, rows: 1 },
-          { title: 'Card 2', cols: 1, rows: 1 },
-          { title: 'Card 3', cols: 1, rows: 1 },
-          { title: 'Card 4', cols: 1, rows: 1 }
-        ];
-      }
-
-      return [
-        { title: 'Card 1', cols: 2, rows: 1 },
-        { title: 'Card 2', cols: 1, rows: 1 },
-        { title: 'Card 3', cols: 1, rows: 2 },
-        { title: 'Card 4', cols: 1, rows: 1 }
-      ];
-    })
-  );
   cards2 = [
       { title: 'Card 1', cols: 2, rows: 1 },
       { title: 'Card 2', cols: 1, rows: 1 },
       { title: 'Card 3', cols: 1, rows: 2 },
       { title: 'Card 4', cols: 1, rows: 1 }
-  ]
+  ];
 
-  constructor(private breakpointObserver: BreakpointObserver) {}
+  content: Content[];
+  posts: Content[];
+
+  constructor(private firstService: FirstService) {}
+
+  ngOnInit() {
+      this.content = this.firstService.getContent();
+      this.posts = this.firstService.getPosts();
+  }
 }
