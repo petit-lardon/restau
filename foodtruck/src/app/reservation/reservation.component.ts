d75f53a6b3f08fa9b6609f59dde9563e2dc8f178
Julien LAVIE <jlavie@leadformance.com>
2018-07-29 23:46:59 +0200

    create form + demo display

--- foodtruck/src/app/reservation/reservation.component.ts
+++ foodtruck/src/app/reservation/reservation.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-reservation',
@@ -6,10 +7,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./reservation.component.css']
 })
 export class ReservationComponent implements OnInit {
+  reservationForm: FormGroup;
+  date = new Date();
 
-  constructor() { }
+  timeOptions = ['12:00', '13:00'];
+  peopleOptions = [1, 2, 3];
+
+  constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit() {
+    this.createForm();
+
   }
 
+  createForm() {
+    this.reservationForm = this.formBuilder.group( {
+        date: new Date(),
+        time: '',
+        people: 1
+    })
+  }
 }
