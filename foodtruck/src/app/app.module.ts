c082628847c13351f8bad97388200ccc14ac2cf0
Julien LAVIE <jlavie@leadformance.com>
2018-08-15 12:09:24 +0200

    provide fr language for datepicker

--- foodtruck/src/app/app.module.ts
+++ foodtruck/src/app/app.module.ts
@@ -7,7 +7,11 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MainNavComponent } from './main-nav/main-nav.component';
 import { LayoutModule } from '@angular/cdk/layout';
-import { MatToolbarModule, MatDatepickerModule, MatInputModule, MatFormFieldModule, MatButtonModule, MatNativeDateModule, MatSidenavModule, MatIconModule, MatListModule, MatCardModule, MatGridListModule, MatMenuModule, MatTableModule, MatPaginatorModule, MatSortModule } from '@angular/material';
+import {
+    MatToolbarModule, MatDatepickerModule, MatInputModule, MatFormFieldModule, MatButtonModule, MatNativeDateModule,
+    MatSidenavModule, MatIconModule, MatListModule, MatCardModule, MatGridListModule, MatMenuModule, MatTableModule,
+    MatPaginatorModule, MatSortModule, MAT_DATE_LOCALE
+} from '@angular/material';
 import { HomeComponent } from './home/home.component';
 import { CarteComponent } from './carte/carte.component';
 import { ReservationComponent } from './reservation/reservation.component';
@@ -56,7 +60,10 @@ const routes: Routes = [
     FormsModule,
     ReactiveFormsModule
   ],
-  providers: [],
+  providers: [
+      {provide: MAT_DATE_LOCALE,
+      useValue: 'fr-FR'}
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
